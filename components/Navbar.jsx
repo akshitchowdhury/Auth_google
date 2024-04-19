27154fa9b2aae22db415be3d2f9ae537fc736c60
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-19 18:09:00 +0530

    auth4

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,12 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
+import { useSession } from 'next-auth/react'
+
 
 const Navbar = () => {
+
+const {status} = useSession()
   return (
     <div className='p-4 flex justify-between items-center
     shadow-md'>
@@ -9,10 +14,22 @@ const Navbar = () => {
       className='font-bold text-lg
       text-blue-700'
       href={'/'}>DevAshura Auth</Link>
-        <button
+        {
+          status=== "authenticated" ? 
+          (
+            <button
+        className='bg-slate-900
+        text-white px-6 py-4
+        rounded-md'>Sign Out</button>
+          ) : (
+            <button
         className='bg-slate-900
         text-white px-6 py-4
         rounded-md'>Sign In</button>
+
+          )
+        }
+        
     </div>
   )
 }
